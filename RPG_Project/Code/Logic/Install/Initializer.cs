6ed2d4c7eb8a6db22d961c2f131ee8cad72113c7
Adam Kopuletý <zerostar362@live.com>
2021-12-17 18:04:49 +0100

    Add project files.

---
+++ RPG_Project/Code/Logic/Install/Initializer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Project.Code.Logic.Install
+{
+    internal class Initializer
+    {
+        string mainDirectoryPath = @"C:\Users\" + Environment.UserName + @"\Documents\RPG";
+        /// <summary>
+        /// Pours init data and create directories
+        /// </summary>
+        private void Initialize() 
+        {
+            bool directories = CheckDirectories(true);
+            CheckAndCreateFiles();
+
+            if(directories == true) 
+            {
+                StartDataStream();
+            }
+        }
+
+        public void InitOnStart() 
+        {
+            if(CheckDirectories() == false) 
+            {
+                Initialize();
+            }
+
+            LoadGameData();
+        }
+
+        /// <summary>
+        /// Loads main game objects
+        /// </summary>
+        private void LoadGameData() 
+        {
+        }
+
+        private void StartDataStream() 
+        {
+        }
+
+        private void CheckAndCreateFiles() 
+        {
+            
+        }
+
+        private bool CheckDirectories(bool create = false) 
+        {
+            if(Directory.Exists(mainDirectoryPath + @"\Custom") & Directory.Exists(mainDirectoryPath + @"\INIT")){ return true; }
+
+            if(!Directory.Exists(mainDirectoryPath + @"\Custom"))
+            {
+                if(create == true) 
+                {
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom\Classes");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom\Characters");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom\Items");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom\Locations");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom\Monsters");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom\Player");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom\Quests");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\Custom\World");
+                }
+            }
+
+            if(!Directory.Exists(mainDirectoryPath + @"\INIT")) 
+            {
+                if(create == true) 
+                {
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT\Classes");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT\Characters");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT\Items");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT\Locations");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT\Monsters");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT\Player");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT\Quests");
+                    Directory.CreateDirectory(mainDirectoryPath + @"\INIT\World");
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ RPG_Project/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Data.SQLite;
+using System.IO;
+
+namespace RPG_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        
+        public void CreateDatabaseAndTable()
+        {
+            SQLiteConnection con;
+            SQLiteCommand cmd;
+            SQLiteDataReader dr;
+
+            if (!File.Exists("MyDatabase.sqlite"))
+            {
+                SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+                string sql = @"CREATE TABLE Student(
+                               ID INTEGER PRIMARY KEY AUTOINCREMENT ,
+                               FirstName           TEXT      NOT NULL,
+                               LastName            TEXT       NOT NULL
+                            );";
+                con = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                con.Open();
+                cmd = new SQLiteCommand(sql, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+            }
+            else
+            {
+                con = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            }
+        }
+
+        public MainWindow()
+        {
+            CreateDatabaseAndTable();
+            InitializeComponent();
+        }
+
+        private void OnClick_ExitButton(object sender, RoutedEventArgs e) 
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
